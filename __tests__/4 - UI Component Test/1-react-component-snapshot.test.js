7e8dcd635b7986dd982b9b3f43484515edd16c7d
Calvin Moore <calvin.moore@allangray.co.za>
2019-05-21 09:33:32 +0200

    one last unit test

--- __tests__/4 - UI Component Test/1-react-component-snapshot.test.js
+++ __tests__/4 - UI Component Test/1-react-component-snapshot.test.js
@@ -7,12 +7,9 @@ test.skip('Application Snapshot', async () => {
   const browser = await puppeteer.launch(/*{ devtools: true }*/);
   const page = await browser.newPage();
   await page.setViewport({ width: 1024, height: 2000 });
-  await page.goto('https://nordictestingdays.eu/schedule');
+  await page.goto('http://www.projectcodex.co/');
   //await page.evaluate(() => { debugger; });
   const image = await page.screenshot({ clip: { x: 0, y: 0, width: 1024, height: 2000 } });
-  expect(image).toMatchImageSnapshot({
-    failureThreshold: '0.5',
-    failureThresholdType: 'percent'
-  });
+  expect(image).toMatchImageSnapshot(/*{ failureThreshold: '0.1', failureThresholdType: 'percent' }*/);
   await browser.close();
 }, 20000);
